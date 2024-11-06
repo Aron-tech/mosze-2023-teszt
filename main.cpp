b9545ef27a7f5f38c74326f3997ca1a33d7e6784
Aron-tech <aron.papp2003@gmail.com>
2024-11-06 11:03:25 +0100

    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    
+    int *b = new int[NELEMENTS]; // Hiba: NELEMENTS helyett N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //Hiba: ';' hiányzik
+    for (int i = 0;) // Hiba: A for hibásan van definiálva.
     {
         b[i] = i * 2;
     }
@@ -15,12 +16,13 @@ int main()
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // Hiba: Az átlag változónak megkell adni kezdő értéket.
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //Hiba: ';' hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //Hiba: Lefoglalt dinamikus memória felszabadítása hiányzik
     return 0;
 }
